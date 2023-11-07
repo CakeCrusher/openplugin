9b81003fbae6fbb7ccddccd73551a8ff6027af03
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2023-11-07 12:06:47 -0500

    upgraded constants

--- npm-core/openplugincore/src/util/constants.ts
+++ npm-core/openplugincore/src/util/constants.ts
@@ -18,4 +18,32 @@ export const openaiModelsInfo: ModelsInfo = {
   'gpt-4-32k-0613': {
     maxTokens: 32768,
   },
+  'gpt-3.5-turbo-1106': {
+    maxTokens: 16000,
+  },
+  'gpt-4-1106-preview': {
+    maxTokens: 128000,
+  },
 };
+
+
+// openai_models_info = {
+//   'gpt-3.5-turbo-0613': {
+//       'max_tokens': 4096,
+//   },
+//   'gpt-3.5-turbo-16k-0613': {
+//       'max_tokens': 16384,
+//   },
+//   'gpt-4-0613': {
+//       'max_tokens': 8192,
+//   },
+//   'gpt-4-32k-0613': {
+//       'max_tokens': 32768,
+//   },
+//   'gpt-3.5-turbo-1106': {
+//       'max_tokens': 16000,
+//   },
+//   'gpt-4-1106-preview': {
+//       'max_tokens': 128000,
+//   },
+// }
